66efad64dd3d103c8c2bd7256f2f0b61e59e6241
admansar <adnane.mansar@etu.uae.ac.ma>
2024-03-25 09:26:32 -0400

    lol

--- Makefile
+++ Makefile
@@ -2,16 +2,19 @@ containers=$(shell sudo docker ps -qa)
 rm_containers = docker container rm -f $(containers)
 images=$(shell sudo docker images -qa)
 rm_images = docker rmi -f $(images)
-user:=$USER
 
-all :
-	mkdir -p /home/admansar/data/
-	docker compose -f srcs/docker-compose.yml up
-	#docker build -t nginx nginx/
-	#docker build -t mariadb mariadb/
+all : up
+
+sudo :
+	@sudo -v
+
+up : sudo
+	mkdir -p /home/admansar/data/mariadb
+	mkdir -p /home/admansar/data/wordpress
+	docker compose -f srcs/docker-compose.yml up -d
 
 stop :
-	docker-compose -f requirements  stop
+	docker compose -f srcs/docker-compose.yml  stop
 
 clean :
 	@if  [ -z "$(containers)" ]; then echo "no containers to delete" ; else  $(rm_containers) ;fi
@@ -19,13 +22,15 @@ clean :
 fclean : clean
 	@if  [ -z "$(images)" ]; then echo "no images to delete"; else  $(rm_images);fi
 	@echo everything is clean no, just go ahead !
-	rm -rf volumes
+	@sudo rm -rf /home/admansar/data
 	
 
 sfclean : prune fclean 
 
 prune :
 	docker buildx prune -af
+	docker system prune --volumes -f
 
-re : fclean all
+re : sudo fclean all
 
+sre : sudo sfclean all
--- srcs/requirements/wordpress/run_wp.sh
+++ srcs/requirements/wordpress/run_wp.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
 
+
+cd /var/www/html
+
 wp core download --allow-root
+wp cli update
 wp config create	--allow-root \
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	--dbhost=mariadb:3306
 
-wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --skip-email --allow-root
+wp core install --url=localhost --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --skip-email --allow-root
 
-#cd /var/www/html 
-#
-#if [ $(ls | wc -l) -ne 0 ]
-#then 
-#	rm -rf * 
-#fi
-#
-#wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-#
-#chmod +x wp-cli.phar
-#
-#mv wp-cli.phar /usr/local/bin/wp
-#
-#
-#wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --skip-check --allow-root
-#wp theme install teluro --activate --allow-root
-#wp user create adnan --role=author --user_pass=adnan --allow-root
-#
-/usr/sbin/php-fpm7.3 -F
-#
+/usr/sbin/php-fpm8.2 -F
+
 #bash
