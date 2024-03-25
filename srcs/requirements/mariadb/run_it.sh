719573d90a1f68978d7a9cb2b60df51564e30b4c
admansar <adnane.mansar@etu.uae.ac.ma>
2024-03-25 14:22:57 -0400

    update

--- srcs/requirements/mariadb/run_it.sh
+++ srcs/requirements/mariadb/run_it.sh
@@ -5,7 +5,7 @@
 echo  "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" >> lol
 echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; " >> lol
 echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%'; " >> lol
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '12345'; " >> lol
+#echo "ALTER USER 'root'@'%' IDENTIFIED BY '12345'; " >> lol
 echo "FLUSH PRIVILEGES;" >> lol
 
 mariadbd --bootstrap --user=root < lol
