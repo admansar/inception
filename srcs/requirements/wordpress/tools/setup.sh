08d73480498e2a8a43abfeb213f9b20742a392c4
admansar <adnane.mansar@etu.uae.ac.ma>
2024-03-28 12:30:50 -0400

    next_step_is_to_recreat_mariadb

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,39 +1,19 @@
-sleep 5
+sleep 5 #just to make sure the mariadb container has finished, remember the container not the image
 
-wordpressInit()
-{
-	cd /var/www/html
-
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-	chmod +x wp-cli.phar
-
-	mv wp-cli.phar /usr/local/bin/wp
-
-	wp --info
-
-	wp --allow-root --path=/var/www/html/ core download
-
-	wp --allow-root --path=/var/www/html/ config create --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} 		--dbhost=mariadb:3306
-
-	wp --allow-root --path=/var/www/html/ core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_name=${ADMIN_NAME}\
-	--admin_password=${ADMIN_PASSWORD} --admin_email=${WP_EMAIL}
-	wp --allow-root user create ${USER_NAME} ${USER_EMAIL} --role=author --user_pass=${WP_PASSWORD}
-
-}
-
-
-if ! [ -f /var/www/html/index.php ];
+if ! [ -f /var/www/html/index.php ]
 then
-	echo "Need to install wordpress"
-	wordpressInit	
+	echo "installing wordpress ..."
+	cd /var/www/html
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+	wp --allow-root --path=/var/www/html/ core download
+	wp --allow-root --path=/var/www/html/ config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb:3306
+	wp --allow-root --path=/var/www/html/ core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_name=$ADMIN_NAME --admin_password=$ADMIN_PASSWORD --admin_email=$WP_EMAIL
+	wp --allow-root user create $USER_NAME $USER_EMAIL --role=author --user_pass=$WP_PASSWORD
 else
-	echo "No need to install wordpress"
+	echo "wordpress is already in"
 fi
-
-ls /wordpressSetUp/tools
-
-ls /var/www/html
 
 php-fpm8.2 -F
 
