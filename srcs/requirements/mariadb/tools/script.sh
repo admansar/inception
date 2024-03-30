639cf6bc872f53c337414649a42cfbb29df7ba55
admansar <adnane.mansar@etu.uae.ac.ma>
2024-03-30 07:35:06 -0400

    lol

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,14 +1,14 @@
-sed -i "s/mysqlUser/${MARIADB_USER}/" /maria/conf/init.sql
+sed -i "s/mysqlUser/${MARIADB_USER}/" /tools/init.maria
 
-sed -i "s/mysqlUserPass/${MARIADB_PASSWORD}/"  /maria/conf/init.sql
+sed -i "s/mysqlUserPass/${MARIADB_PASSWORD}/"  /tools/init.maria
 
-sed -i "s/mysqlRootPass/${MARIADB_ROOT_PASSWORD}/" /maria/conf/init.sql
+sed -i "s/mysqlRootPass/${MARIADB_ROOT_PASSWORD}/" /tools/init.maria
 
-sed -i "s/mysqlDb/${WP_DATABASE}/" /maria/conf/init.sql
+sed -i "s/mysqlDb/${WP_DATABASE}/" /tools/init.maria
 
-sed -i "s/mysqlTable/${MYSQL_TABLE_NAME}/" /maria/conf/init.sql
+sed -i "s/mysqlTable/${MYSQL_TABLE_NAME}/" /tools/init.maria
 
-mariadbd --bootstrap --user=root < /maria/conf/init.sql
+mariadbd --bootstrap --user=root < /tools/init.maria
 
 
 mariadbd --user=root
