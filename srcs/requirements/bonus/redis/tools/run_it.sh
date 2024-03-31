997f549ddb5f8548eb9d7a2803ac38d5d01b8acc
admansar <adnane.mansar@etu.uae.ac.ma>
2024-03-31 13:31:00 -0400

    redis_on

---
+++ srcs/requirements/bonus/redis/tools/run_it.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "maxmemory 256mb" >> /etc/redis/redis.conf
+echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf
+
+sed -i "s/127.0.0.1/0.0.0.0" /etc/redis/redis.conf
+
+redis-server --protected-mode no
