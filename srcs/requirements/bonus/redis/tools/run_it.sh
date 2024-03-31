8cddc8b0ee010360e27930098f46d5cfbb991a17
admansar <adnanmansar.am@gmail.com>
2024-03-31 23:00:19 +0000

    lol

--- srcs/requirements/bonus/redis/tools/run_it.sh
+++ srcs/requirements/bonus/redis/tools/run_it.sh
@@ -3,6 +3,23 @@
 echo "maxmemory 256mb" >> /etc/redis/redis.conf
 echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf
 
-sed -i "s/127.0.0.1/0.0.0.0" /etc/redis/redis.conf
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/redis/redis.conf
+
+
+if [ ! -f /var/www/html/wp-config.php ]
+then
+	echo waiting for config.php to be created for wordpress;
+	stime=$(date +%s);
+	while [ ! -f /var/www/html/wp-config.php ]
+	do
+		sleep 1;
+	done
+	etime=$(date +%s);
+	echo waited for $((etime - stime)) seconds , finishing the redis server ....
+	echo "define('WP_CACHE_KEY_SALT', 'admansar.42.fr');" >> /var/www/html/wp-config.php
+	echo "define('WP_CACHE', true);" >> /var/www/html/wp-config.php
+fi
+
+
 
 redis-server --protected-mode no
