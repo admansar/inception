01a10a8b0921c2371d531aa8615d110623a3d1e1
admansar <adnane.mansar@etu.uae.ac.ma>
2024-04-03 14:31:49 -0400

    gg

--- srcs/requirements/bonus/ftp-server/tools/ftp.sh
+++ srcs/requirements/bonus/ftp-server/tools/ftp.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo $FTP_PASSWORD >> lol 
-echo $FTP_PASSWORD >> lol 
-adduser $FTP_USER < lol
-usermod -d /var/www/html/ $FTP_USER
 vim -s /ftp_conf.vim /etc/vsftpd.conf
-rm lol
+mkdir -p /var/run/vsftpd/empty
+useradd $FTP_USER
+mkdir /home/$FTP_USER
+chmod 755 /home/$FTP_USER
+echo $FTP_USER:$FTP_PASS | chpasswd 
+usermod -d /var/www/html/ $FTP_USER
+echo -e "\033[0;32m"
+echo '<---------------------------------------------------------------------------------------->'
+echo "|for easy connection, install filezilla and connect using $(hostname -I) as a host name ... |"
+echo '<---------------------------------------------------------------------------------------->'
+echo -e "\033[0m"
+
 vsftpd
